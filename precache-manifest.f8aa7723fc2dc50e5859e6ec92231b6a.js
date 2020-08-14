aa8c9ab29472aac90a92faffb5bba2399dc0b535
José González <deliriousdelta@gmail.com>
2020-08-14 18:58:55 -0500

    Updates

---
+++ precache-manifest.f8aa7723fc2dc50e5859e6ec92231b6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a03a02136ba7169c587a578f80de3a3",
+    "url": "/proyecto-b43/index.html"
+  },
+  {
+    "revision": "1df1a8cb80b51008f7fb",
+    "url": "/proyecto-b43/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "023e882bc354e90fb457",
+    "url": "/proyecto-b43/static/css/main.5c3f5e52.chunk.css"
+  },
+  {
+    "revision": "1df1a8cb80b51008f7fb",
+    "url": "/proyecto-b43/static/js/2.9290e65c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/proyecto-b43/static/js/2.9290e65c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "023e882bc354e90fb457",
+    "url": "/proyecto-b43/static/js/main.44f9834f.chunk.js"
+  },
+  {
+    "revision": "884deb4696c7606cf8dd",
+    "url": "/proyecto-b43/static/js/runtime-main.cf8cbc2a.js"
+  }
+]);
