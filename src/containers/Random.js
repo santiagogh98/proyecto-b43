ac1bb8da5d681376ff3d579ac6854593f6b02c27
José González <deliriousdelta@gmail.com>
2020-08-14 18:10:24 -0500

    Update Random.js

--- src/containers/Random.js
+++ src/containers/Random.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Card from '../components/Card';
 import CardActivity from '../components/CardActivity';
-import axios from 'axios';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import './Random.css'
